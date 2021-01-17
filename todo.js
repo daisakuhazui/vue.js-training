826a518e5285fc5418653bf7a99b0201e891bcdc
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2021-01-17 15:41:43 +0900

    Load local storage

--- todo.js
+++ todo.js
@@ -1,17 +1,16 @@
 const vm = new Vue({
   el: '#app',
   data: {
-    items: [
-        { title: '領収書を準備する', isChecked: true },
-        { title: 'Vue.jsハンズオンの資料を作る', isChecked: true },
-        { title: '参加者の人数を確認する', isChecked: false },
-        { title: 'ピザを注文する', isChecked: false },
-        { title: '参加費のお釣りを準備する', isChecked: false },
-        { title: '会場設営をする', isChecked: false },
-    ],
+    items: [],
     newItemTitle: ''
   },
   methods: {
+    loadTodo: function(){
+      this.items = JSON.parse( localStorage.getItem('items') );
+      if( !this.items ){
+        this.items = [];
+      }
+    },
     addTodo: function(){
       this.items.push({
         title: this.newItemTitle,
@@ -29,5 +28,8 @@ const vm = new Vue({
     saveTodo: function(){
       localStorage.setItem('items', JSON.stringify(this.items));
     },
-  }
+  },
+  mounted: function(){
+    this.loadTodo();
+  },
 })
